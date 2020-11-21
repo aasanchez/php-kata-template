36a05545034a201b3e59b67da99776bfdfbe805b
Alexis Sánchez <aasanchez@gmail.com>
2020-11-21 18:03:31 +0100

    First commit

---
+++ src/Calc.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kata;
+
+/**
+ * Class Bowling
+ */
+class Calc
+{
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * This function calculateAdd: Sum of two element
+     *
+     * @param float $first First element in the sum.
+     * @param float $second Second element in the sum.
+     *
+     * @return float
+     */
+    public function addition(float $first, float $second): float
+    {
+        return ($first + $second);
+    }
+}
