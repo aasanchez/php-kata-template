9994d2f5a4feb7fc407960194c08624964eb07f6
Alexis Sánchez <aasanchez@gmail.com>
2020-11-21 18:03:31 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PHP Kata Template
+
+![PHP Kata Template](https://github.com/aasanchez/php-kata-template/workflows/PHP-Kata/badge.svg)
+[![SymfonyInsight](https://insight.symfony.com/projects/c52b6e23-683f-4e38-8f79-99d5586546e3/mini.svg)](https://insight.symfony.com/projects/c52b6e23-683f-4e38-8f79-99d5586546e3)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/aasanchez/php-kata-template/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/aasanchez/php-kata-template/?branch=master)
+[![Code Coverage](https://scrutinizer-ci.com/g/aasanchez/php-kata-template/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/aasanchez/php-kata-template/?branch=master)
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/376159da7a5c4b6f9039bc65fbd82555)](https://www.codacy.com/gh/aasanchez/php-kata-template/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=aasanchez/php-kata-template&amp;utm_campaign=Badge_Grade)
---
+++ tests/CalcTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Kata\Tests;
+
+use Kata\Calc;
+use PHPUnit\Framework\TestCase;
+use TypeError;
+
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
+class CalcTest extends TestCase
+{
+    protected Calc $calc;
+
+    public function setUp(): void
+    {
+        $this->calc = new Calc();
+        parent::setUp();
+    }
+
+    public function testClassImplementation(): void
+    {
+        $this->assertInstanceOf(\Kata\Calc::class, $this->calc);
+    }
+
+    /**
+     * @test
+     * @dataProvider validAdditionProvider
+     * @param float $first
+     * @param float $second
+     * @param float $expectedResult
+     *
+     * @backupStaticAttributes enabled
+     */
+    public function validAddition(float $first, float $second, float $expectedResult): void
+    {
+        $this->assertEquals(
+            $expectedResult,
+            (new Calc())->Addition($first, $second)
+        );
+    }
+
+    /**
+     * @test2
+     * return  array<Foo>
+     */
+    public function validAdditionProvider(): array
+    {
+        return [
+            [1, 1.1, 2.1],
+            [1, 1, 2],
+            [1, 0, 1],
+            [1, -1, 0],
+            [1, -1.1, -0.1],
+            [0, 1.1, 1.1],
+            [0, 1, 1],
+            [0, 0, 0],
+            [0, -1, -1],
+            [0, -1.1, -1.1],
+            [-1, 1.1, 0.1],
+            [-1, 1, 0],
+            [-1, 0, -1],
+            [-1, -1, -2],
+            [-1, -1.1, -2.1],
+            [-1.1, 1.1, 0],
+            [-1.1, 1, -0.1],
+            [-1.1, 0, -1.1],
+            [-1.1, -1, -2.1],
+            [-1.1, -1.1, -2.2],
+        ];
+    }
+
+    /**
+     * @test
+     * @psalm-suppress NullArgument
+     */
+    public function invalidAdditionNullParameter(): void
+    {
+        $this->expectException(TypeError::class);
+        $this->calc->addition(null, null); // @phpstan-ignore-line
+    }
+
+    /**
+     * @test
+     * @psalm-suppress InvalidScalarArgument
+     */
+    public function invalidAdditionStringParameter(): void
+    {
+        $this->expectException(TypeError::class);
+        $this->calc->addition('string', 1); // @phpstan-ignore-line
+    }
+
+    /**
+     * @test
+     * @psalm-suppress InvalidArgument
+     */
+    public function invalidAdditionArrayParameter(): void
+    {
+        $this->expectException(TypeError::class);
+        $this->calc->addition([1], 1); // @phpstan-ignore-line
+    }
+}
